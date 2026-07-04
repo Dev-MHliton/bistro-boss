65e49d970f2241f5ae2a7a62544da599302ab0bc
Dev-MHliton <liton41126nk@gmail.com>
2026-07-05 04:57:17 +0600

    Bistro boss space

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -2,10 +2,8 @@ import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
-
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
-
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
@@ -31,7 +29,6 @@ const AuthProvider = ({ children }) => {
             displayName: name, photoURL: photo
         });
     }
-
     useEffect(() => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
